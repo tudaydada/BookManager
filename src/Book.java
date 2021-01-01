93f1a770d9230bca51317f02f817466f85d8ab97
pvhunglop91 <76692287+pvhunglop91@users.noreply.github.com>
2021-01-01 23:12:19 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,111 +1,96 @@
 public class Book {
-	private String ten, ID, tacGia, theLoai, NXB;
-	private int giaTien, soLuong, daBan;
+    private String ten, ID, tacGia, theLoai, NXB;
+    private int giaTien, soLuong, daBan;
 
-	public Book() {
-		
-	}
-	
-	public Book(String ten, String iD, String tacGia, String theLoai, String nXB, int giaTien, int soLuong, int daBan) {
-		super();
-		this.ten = ten;
-		this.ID = iD;
-		this.tacGia = tacGia;
-		this.theLoai = theLoai;
-		this.NXB = nXB;
-		this.giaTien = giaTien;
-		this.soLuong = soLuong;
-		this.daBan = daBan;
-	}
+    public Book() {
 
-	public String getTen() {
-		return ten;
-	}
+    }
 
-	public void setTen(String ten) {
-		this.ten = ten;
-	}
+    public Book(String iD, int soLuong) {
+        super();
+        ID = iD;
+        this.soLuong = soLuong;
+    }
 
-	public String getID() {
-		return ID;
-	}
+    public Book(String ten, String iD, String tacGia, String theLoai, String nXB, int giaTien, int soLuong, int daBan) {
+        super();
+        this.ten = ten;
+        this.ID = iD;
+        this.tacGia = tacGia;
+        this.theLoai = theLoai;
+        this.NXB = nXB;
+        this.giaTien = giaTien;
+        this.soLuong = soLuong;
+        this.daBan = daBan;
+    }
 
-	public void setID(String iD) {
-		ID = iD;
-	}
+    public String getTen() {
+        return ten;
+    }
 
-	public String getTacGia() {
-		return tacGia;
-	}
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
 
-	public void setTacGia(String tacGia) {
-		this.tacGia = tacGia;
-	}
+    public String getID() {
+        return ID;
+    }
 
-	public String getTheLoai() {
-		return theLoai;
-	}
+    public void setID(String iD) {
+        ID = iD;
+    }
 
-	public void setTheLoai(String theLoai) {
-		this.theLoai = theLoai;
-	}
+    public String getTacGia() {
+        return tacGia;
+    }
 
-	public String getNXB() {
-		return NXB;
-	}
+    public void setTacGia(String tacGia) {
+        this.tacGia = tacGia;
+    }
 
-	public void setNXB(String nXB) {
-		NXB = nXB;
-	}
+    public String getTheLoai() {
+        return theLoai;
+    }
 
-	public int getGiaTien() {
-		return giaTien;
-	}
+    public void setTheLoai(String theLoai) {
+        this.theLoai = theLoai;
+    }
 
-	public void setGiaTien(int giaTien) {
-		this.giaTien = giaTien;
-	}
+    public String getNXB() {
+        return NXB;
+    }
 
-	public int getSoLuong() {
-		return soLuong;
-	}
+    public void setNXB(String nXB) {
+        NXB = nXB;
+    }
 
-	public void setSoLuong(int soLuong) {
-		this.soLuong = soLuong;
-	}
+    public int getGiaTien() {
+        return giaTien;
+    }
 
-	public int getDaBan() {
-		return daBan;
-	}
+    public void setGiaTien(int giaTien) {
+        this.giaTien = giaTien;
+    }
 
-	public void setDaBan(int daBan) {
-		this.daBan = daBan;
-	}
-	
-	public void display() {
-		System.out.println("\n ID : " + ID);
-		System.out.print("/Ten sach : " + ten);
-		System.out.print("/Tac gia : " + tacGia);
-		System.out.print("/The Loai : " + theLoai);
-		System.out.print("/Nha Xuat Ban : " + NXB);
-		System.out.print("/Gia tien : " + giaTien);
-		System.out.print("/So luong nhap ve : " + soLuong);
-		System.out.print("/So luong da ban : " + daBan);
-		System.out.print("/So luong con lai : " + (int)(soLuong - daBan));
-	}
+    public int getSoLuong() {
+        return soLuong;
+    }
 
-	@Override
-	public String toString(){
-		return "Book {" +
-				"ten = '" + ten + '\'' +
-				", ID = '" + ID + '\'' +
-				", tacGia = '" + tacGia + '\'' +
-				", theLoai = '" + theLoai + '\'' +
-				", NXB = '" + NXB + '\'' +
-				", giaTien = " + giaTien +
-				", soLuong = " + soLuong +
-				", daBan = " + daBan +
-				'}';
-	}
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public int getDaBan() {
+        return daBan;
+    }
+
+    public void setDaBan(int daBan) {
+        this.daBan = daBan;
+    }
+
+    public void display(){
+        System.out.println("Book [Ten : " + ten + "/ ID : " + ID + "/ Tac Gia : " + tacGia +
+                "/ The Loai : " + theLoai + "/ NXB : " + NXB + "/ Gia Tien : " + giaTien +
+                "/ So Luong : " + soLuong + "/ Da Ban : " + daBan + "]");
+    }
 }
-
