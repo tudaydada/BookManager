43207916b04aca4c9e91e983e18005d387bc87dd
pvhunglop91 <76692287+pvhunglop91@users.noreply.github.com>
2021-01-01 23:11:44 +0700

    Update Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -1,56 +1,38 @@
 import java.util.ArrayList;
 
 public class Customer extends Person {
-	private String ID, soDT;
-	private int amount;
-	ArrayList<Book> sachDaMua;
-	
-	public Customer() {
-		
-	}
+    private String ID, soDT;
 
-	public int getAmount() {
-		return amount;
-	}
+    public Customer() {
 
-	public void setAmount(int amount) {
-		this.amount = amount;
-	}
+    }
 
-	public Customer(String iD, String ten, int tuoi, String soDT, String diaChi) {
-		super(ten, diaChi, tuoi);
-		ID = iD;
-		this.soDT = soDT;
+    public Customer(String iD, String ten, int tuoi, String soDT, String diaChi) {
+        super(ten, diaChi, tuoi);
+        ID = iD;
+        this.soDT = soDT;
 
-	}
+    }
 
-	public String getID() {
-		return ID;
-	}
+    public String getID() {
+        return ID;
+    }
 
-	public void setID(String iD) {
-		ID = iD;
-	}
+    public void setID(String iD) {
+        ID = iD;
+    }
 
-	public String getSoDT() {
-		return soDT;
-	}
+    public String getSoDT() {
+        return soDT;
+    }
 
-	public void setSoDT(String soDT) {
-		this.soDT = soDT;
-	}
+    public void setSoDT(String soDT) {
+        this.soDT = soDT;
+    }
 
-	public ArrayList<Book> getSachDaMua() {
-		return sachDaMua;
-	}
-
-	public void setSachMua(Book sachDaMua) {
-		this.sachDaMua.add(sachDaMua);
-	}
-	public void display() {
-		System.out.println("[ID: " + ID+"/"+"Ten khach hang : " + this.getTen()+"/"+"tuoi : " + this.getTuoi()+"/"+"dia chi : "
-				+ this.getDiaChi()+"/"+"so dien thoai : " + soDT+"/"+"so lan mua : " + amount+"/sach da mua: "+sachDaMua+"]");
-
-
-	}
+    public void display() {
+        System.out.println("Custommer [ID : " + ID + "/ Ten khach hang : " + this.getTen() +
+                "/ Tuoi : " + this.getTuoi() + "/ Dia chi : " + this.getDiaChi() +
+                "/ So dien thoai : " + soDT + "]");
+    }
 }
