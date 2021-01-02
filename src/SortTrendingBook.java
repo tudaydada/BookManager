4b67cfdc82cfa01cdbf474fdbc9c343f700a918c
pvhunglop91 <76692287+pvhunglop91@users.noreply.github.com>
2021-01-02 16:13:35 +0700

    Update SortTrendingBook.java

--- src/SortTrendingBook.java
+++ src/SortTrendingBook.java
@@ -2,13 +2,16 @@ import java.util.Comparator;
 
 public class SortTrendingBook implements Comparator<Book> {
 
-	@Override
-	public int compare(Book o1,Book o2) {
-		int tam1 = o1.getSoLuong()-o1.getDaBan();
-		int tam2 = o2.getSoLuong()-o2.getDaBan();
-		if(tam1>tam2) return -1;
-		else if(tam1<tam2) return 1;
-		return 0;
-	}
+    @Override
+    public int compare(Book o1, Book o2) {
+        int tam1 = o1.getDaBan();
+        int tam2 = o2.getDaBan();
+        if (tam1 > tam2) {
+            return -1;
+        } else if (tam1 < tam2) {
+            return 1;
+        }
+        return 0;
+    }
 
 }
