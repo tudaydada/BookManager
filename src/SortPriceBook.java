760a0a4bd3b9fd5b557c7bc845019105ef897356
pvhunglop91 <76692287+pvhunglop91@users.noreply.github.com>
2021-01-02 16:13:18 +0700

    Update SortPriceBook.java

--- src/SortPriceBook.java
+++ src/SortPriceBook.java
@@ -2,11 +2,14 @@ import java.util.Comparator;
 
 public class SortPriceBook implements Comparator<Book> {
 
-	@Override
-	public int compare(Book o1, Book o2) {
-		if(o1.getGiaTien()>o2.getGiaTien()) return -1;
-		else if(o1.getGiaTien()<o2.getGiaTien()) return 1;
-		return 0;
-	}
-	
+    @Override
+    public int compare(Book o1, Book o2) {
+        if(o1.getGiaTien() < o2.getGiaTien()) {
+            return -1;
+        } else if (o1.getGiaTien() < o2.getGiaTien()) {
+            return 1;
+        }
+        return 0;
+    }
+
 }
