a63129578e417b3e0352645cbf42d336ba526f6f
pvhunglop91 <76692287+pvhunglop91@users.noreply.github.com>
2021-01-02 16:12:18 +0700

    Update SortNameBook.java

--- src/SortNameBook.java
+++ src/SortNameBook.java
@@ -2,12 +2,13 @@ import java.util.Comparator;
 
 public class SortNameBook implements Comparator<Book>{
 
-	@Override
-	public int compare(Book o1, Book o2) {
-		if(o1.getTen().compareTo(o2.getTen())>0) return -1;
-		else if(o1.getTen().compareTo(o2.getTen())<0) return 1;
-		return 0;
-	}
-
-
+    @Override
+    public int compare(Book o1, Book o2) {
+        if (o1.getTen().compareTo(o2.getTen()) < 0) {
+            return -1;
+        } else if (o1.getTen().compareTo(o2.getTen()) < 0) {
+            return 1;
+        }
+        return 0;
+    }
 }
