dc2efcaf09494a7e43b7b9c7f8ebafa455b3f374
pvhunglop91 <76692287+pvhunglop91@users.noreply.github.com>
2021-01-02 16:12:58 +0700

    Update SortNameCustomer.java

--- src/SortNameCustomer.java
+++ src/SortNameCustomer.java
@@ -2,12 +2,16 @@ import java.util.Comparator;
 
 public class SortNameCustomer implements Comparator<Customer>{
 
-	@Override
-	public int compare(Customer o1, Customer o2) {
-		if(o1.getTen().compareTo(o2.getTen())>0) return -1;
-		else if(o1.getTen().compareTo(o2.getTen())<0) return 1;
-		return 0;
-	}
+    @Override
+    public int compare(Customer o1, Customer o2) {
+        if (o1.getTen().compareTo(o2.getTen()) < 0) {
+            return -1;
+        } else if (o1.getTen().compareTo(o2.getTen()) < 0) {
+            return 1;
+        }
+        return 0;
+    }
 
 
 }
+
